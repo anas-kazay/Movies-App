57157625eb7699f649325d497bc13985277f6e2b
anas-kazay <anaskazay@gmail.com>
2024-04-20 12:26:41 +0100

    first commit

---
+++ movies-backend/src/test/java/kazay/anas/moviesbackend/MoviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kazay.anas.moviesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
