1fd054ec03ae92417a408ca043f7bbd231e3bf26
anas-kazay <anaskazay@gmail.com>
2024-04-21 07:58:30 +0100

    finish backend

--- movies-backend/src/main/java/kazay/anas/moviesbackend/MoviesBackendApplication.java
+++ movies-backend/src/main/java/kazay/anas/moviesbackend/MoviesBackendApplication.java
@@ -2,6 +2,8 @@ package kazay.anas.moviesbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoviesBackendApplication {
@@ -9,5 +11,7 @@ public class MoviesBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesBackendApplication.class, args);
 	}
+
+
 
 }
